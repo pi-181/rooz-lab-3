310dad3f6fe1b6b30447fffeda3a20a755f228ab
Max Demydenko <demkom58@gmail.com>
2022-02-12 23:16:04 +0200

    feat: init commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
