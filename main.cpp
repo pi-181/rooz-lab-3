ea44767a6154f8dda107abc89d1e89069bc846c5
Max Demydenko <demkom58@gmail.com>
2022-02-19 17:00:30 +0200

    feat: done all tasks

--- main.cpp
+++ main.cpp
@@ -159,7 +159,43 @@ cv::Mat ColorDetector::process(const cv::Mat &image) {
     return result;
 }
 
+void detectHScolor(const cv::Mat& image, // input image
+                   double minHue, double maxHue, // Hue interval
+                   double minSat, double maxSat, // saturation interval
+                   cv::Mat& mask) { // output mask
+    // convert into HSV space
+    cv::Mat hsv;
+    cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+    // split the 3 channels into 3 images
+    std::vector<cv::Mat> channels;
+    cv::split(hsv, channels);
+
+    // Hue masking
+    cv::Mat mask1; // under maxHue
+    cv::threshold(channels[0], mask1, maxHue, 255,cv::THRESH_BINARY_INV);
+    cv::Mat mask2; // over minHue
+    cv::threshold(channels[0], mask2, minHue, 255,cv::THRESH_BINARY);
+    cv::Mat hueMask; // hue mask
+
+    if (minHue < maxHue)
+        hueMask = mask1 & mask2;
+    else // if interval crosses the zero-degree axis
+        hueMask = mask1 | mask2;
+
+    // under maxSat
+    cv::threshold(channels[1], mask1, maxSat, 255,cv::THRESH_BINARY_INV);
+    // over minSat
+    cv::threshold(channels[1], mask2, minSat, 255,cv::THRESH_BINARY);
+
+    cv::Mat satMask; // saturation mask
+    satMask = mask1 & mask2;
+
+    // combined mask
+    mask = hueMask & satMask;
+}
+
 const cv::String picPath = R"(pics\1.jpg)";
+const cv::String photoPath = R"(pics\2.jpg)";
 
 int main() {
     ColorDetector cdetect;
@@ -173,8 +209,8 @@ int main() {
     // В якості міри приналежності кольору заданому використовувати різницю значень кольорів пікселів
     cdetect.setTargetColor(0xe7, 0xbb, 0x9e);
 
-//    namedWindow("Using absdiff Image", WINDOW_AUTOSIZE);
-//    imshow("Using absdiff Image", cdetect.process(image));
+    namedWindow("Using absdiff Image", WINDOW_AUTOSIZE);
+    imshow("Using absdiff Image", cdetect.process(image));
 
     // 3. Виконати розпізнавання кольору на зображенні, використовуючи функцію floodFill.
     Mat floodFillImage = image.clone();
@@ -186,35 +222,35 @@ int main() {
                   cv::Scalar(35, 35, 35),     // most of the time will be identical
                   cv::FLOODFILL_FIXED_RANGE); // pixels are compared to seed color
 
-//    cv::namedWindow("Flood Fill result");
-//    cv::imshow("Flood Fill result", floodFillImage);
+    cv::namedWindow("Flood Fill result");
+    cv::imshow("Flood Fill result", floodFillImage);
 
     // 4. Виконати сегментацію зображення за допомогою алгоритму GrabCut.
 
-//    const Rect2i foregroundRect = Rect(0, 250, 366, 399);
-//
-//    Mat marked = image.clone();
-//    rectangle(marked, foregroundRect, Scalar(0, 0, 255));
-//    imshow("Selected area", marked);
-//
-//    cv::Mat result; // segmentation (4 possible values)
-//    cv::Mat bgModel,fgModel;
-//
-//    cv::grabCut(image, // input image
-//                result, // segmentation result
-//                foregroundRect,// rectangle containing foreground
-//                bgModel, fgModel, // models
-//                5, // number of iterations
-//                cv::GC_INIT_WITH_RECT); // use rectangle
-//
-//    // Get the pixels marked as likely foreground
-//    cv::compare(result,cv::GC_PR_FGD,result,cv::CMP_EQ);
-//    // Generate output image
-//    cv::Mat foreground(image.size(),CV_8UC3,cv::Scalar(255,255,255));
-//    image.copyTo(foreground, result);
-//
-//    cv::namedWindow("GrabCut result");
-//    cv::imshow("GrabCut result", foreground);
+    const Rect2i foregroundRect = Rect(0, 250, 366, 399);
+
+    Mat marked = image.clone();
+    rectangle(marked, foregroundRect, Scalar(0, 0, 255));
+    imshow("Selected area", marked);
+
+    cv::Mat result; // segmentation (4 possible values)
+    cv::Mat bgModel,fgModel;
+
+    cv::grabCut(image, // input image
+                result, // segmentation result
+                foregroundRect,// rectangle containing foreground
+                bgModel, fgModel, // models
+                5, // number of iterations
+                cv::GC_INIT_WITH_RECT); // use rectangle
+
+    // Get the pixels marked as likely foreground
+    cv::compare(result,cv::GC_PR_FGD,result,cv::CMP_EQ);
+    // Generate output image
+    cv::Mat foreground(image.size(),CV_8UC3,cv::Scalar(255,255,255));
+    image.copyTo(foreground, result);
+
+    cv::namedWindow("GrabCut result");
+    cv::imshow("GrabCut result", foreground);
 
     // 5. Виконати розпізнавання кольору для зображення у колірних системах LAB та HSV.
 
@@ -224,8 +260,8 @@ int main() {
     ColorDetector cldetect(1);
     cldetect.setTargetColor(0xe7, 0xbb, 0x9e);
 
-//    namedWindow("Lab detect Image", WINDOW_AUTOSIZE);
-//    imshow("Lab detect Image", cldetect.process(labImg));
+    namedWindow("Lab detect Image", WINDOW_AUTOSIZE);
+    imshow("Lab detect Image", cldetect.process(labImg));
 
     Mat hsvImg;
     cvtColor(image, hsvImg, COLOR_BGR2HSV);
@@ -233,17 +269,46 @@ int main() {
     ColorDetector chdetect(2);
     chdetect.setTargetColor(0xe7, 0xbb, 0x9e);
 
-//    namedWindow("HSV detect Image", WINDOW_AUTOSIZE);
-//    imshow("HSV detect Image", chdetect.process(labImg));
+    namedWindow("HSV detect Image", WINDOW_AUTOSIZE);
+    imshow("HSV detect Image", chdetect.process(labImg));
 
     // 6.	Встановити постійну яскравість усім пікселям зображення в системі HSV.
     // Відобразіть результат та його гістограму.
 
+    std::vector<cv::Mat> channels;
+    cv::split(hsvImg,channels);
 
+    // Канал значення = 255 для всіх пікселів
+    channels[2]= 255;
+
+    Mat hsvFixedValueImage;
+    cv::merge(channels, hsvFixedValueImage);
+
+    cv::Mat fixedValueImage;
+    cv::cvtColor(hsvFixedValueImage, fixedValueImage, COLOR_HSV2BGR);
+
+    namedWindow("Fixed Value Image", WINDOW_AUTOSIZE);
+    imshow("Fixed Value Image", fixedValueImage);
 
     // 7. Виконати розпізнавання тону шкіри.
 
+    Mat photo = imread(photoPath);
+    resize(photo, photo, Size(photo.cols / 6, photo.rows / 6));
 
+    imshow("7. Source Image", photo);
+
+    cv::Mat mask;
+    detectHScolor(photo,
+                  160, 10, // hue from 320 degrees to 20 degrees
+                  25, 166, // saturation from ~0.1 to 0.65
+                  mask);
+
+    // show masked image
+    cv::Mat detected(photo.size(), CV_8UC3, cv::Scalar(0, 0, 0));
+    photo.copyTo(detected, mask);
+
+    namedWindow("7. Skin Image", WINDOW_AUTOSIZE);
+    imshow("7. Skin Image", detected);
 
     waitKey(0);
     waitKey(0);
